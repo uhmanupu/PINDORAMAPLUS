beb1c4050dd9c6dd914a77170053b62482c95cf8
uhmanupu <emanuelpu@gmail.com>
2025-12-16 10:10:36 -0300

    Delete components directory

--- components/Icons.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Home, Search, Plus, Star, Tv, Film, Play, X, Mic, Sparkles } from 'lucide-react';
-
-export { Home, Search, Plus, Star, Tv, Film, Play, X, Mic, Sparkles };
